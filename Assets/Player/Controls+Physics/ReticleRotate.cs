4329b826b7e240e699931e19e33fb248b043fd08
KingBarou13 <113718439+KingBarou13@users.noreply.github.com>
2024-09-09 12:25:29 +0100

    Minor changes

--- Assets/Player/Controls+Physics/ReticleRotate.cs
+++ Assets/Player/Controls+Physics/ReticleRotate.cs
@@ -6,15 +6,9 @@ using DG.Tweening;
 public class ReticleRotate : MonoBehaviour
 {
     [SerializeField] private float cycleLength = 2;
-    // Start is called before the first frame update
+
     void Start()
     {
         transform.DORotate(new Vector3(0, 0, 360), cycleLength * 0.5f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
