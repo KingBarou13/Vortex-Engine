adb6201a48aecbc5fe31ad77613b9dd97b4efcbf
KingBarou13 <113718439+KingBarou13@users.noreply.github.com>
2024-09-28 16:19:27 +0100

    Jump ball fx added; Jump panel added with path caulculator (same as spring as the both have pretty much the same function)

--- Assets/Player/Controls+Physics/Actions/JumpAction.cs
+++ Assets/Player/Controls+Physics/Actions/JumpAction.cs
@@ -3,26 +3,25 @@ using UnityEngine.InputSystem;
 
 public class JumpAction : PlayerAction
 {
-    [SerializeField] private GrindAction grindAction;  
     [SerializeField] private RisingAndFalling risingAndFalling;
-
     [SerializeField] int jumps;
     [SerializeField] float jumpForce;
     [SerializeField] float airJumpForce;
     [SerializeField] private Animator animator;
+    [SerializeField] private GameObject spinBall;
+    [SerializeField] private GameObject spinFX;
     int currentJumps;
 
     void Start()
     {
-        if (grindAction == null)
-        {
-            grindAction = GetComponentInParent<GrindAction>() ?? GetComponentInChildren<GrindAction>();
-        }
-
         if (risingAndFalling == null)
         {
             risingAndFalling = GetComponent<RisingAndFalling>();
         }
+
+        // Ensure spinBall and spinFX are hidden at the start
+        spinBall.SetActive(false);
+        spinFX.SetActive(false);
     }
 
     public void OnJump(InputAction.CallbackContext callbackContext)
@@ -47,17 +46,16 @@ public class JumpAction : PlayerAction
     {
         currentJumps = jumps;
         animator.SetBool("IsJumping", false);
+
+        // Hide spinBall and spinFX when grounded
+        spinBall.SetActive(false);
+        spinFX.SetActive(false);
+
         risingAndFalling.OnJumpEnded();
     }
 
     void Jump()
     {
-        // Exit rail grind if jumping while on a rail
-        if (grindAction != null && grindAction.onRail)
-        {
-            grindAction.ExitRailOnJump();
-        }
-
         if (currentJumps <= 0) return;
 
         currentJumps--;
@@ -70,6 +68,10 @@ public class JumpAction : PlayerAction
 
         // Update animator to indicate jumping
         animator.SetBool("IsJumping", true);
+
+        // Show spinBall and spinFX when jumping
+        spinBall.SetActive(true);
+        spinFX.SetActive(true);
 
         // Notify Rising and Falling script that a jump has been initiated
         risingAndFalling.OnJumpInitiated();
---
+++ Assets/Player/Controls+Physics/Actions/SpinFX.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SpinFX : MonoBehaviour
+{
+    [SerializeField] private float spinSpeed;
+    [SerializeField] private Transform referenceObject; // Reference to the object to match y-rotation
+
+    private Tween spinTween;
+
+    void Start()
+    {
+        // Start spinning the object around the x-axis
+        spinTween = transform.DORotate(new Vector3(360, 0, 0), spinSpeed * 0.5f, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Restart)
+            .SetEase(Ease.Linear);
+    }
+
+    void Update()
+    {
+        // Update y-rotation to match the reference object's y-rotation
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(currentRotation.x, referenceObject.rotation.eulerAngles.y, currentRotation.z);
+    }
+}
